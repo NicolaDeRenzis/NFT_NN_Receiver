5943d5baef770abdfe4f6246954129ab9f99ed53
s161172 <s161172@student.dtu.dk>
2019-02-26 13:43:01 +0100

    cluster submission scripts update

--- code/NN_train_1/sweepsubmitcpu.sh
+++ code/NN_train_1/sweepsubmitcpu.sh
@@ -1,22 +1,21 @@
-### SCRIPT FOR SUBSMISSION TO SINGLE JOB TO GPU
+### SCRIPT FOR SUBSMISSION OF SWEEP JOBS TO CPU
 
 #!/bin/sh
-#BSUB -q gpuk80
-#BSUB -J NLIN
+#BSUB -q hpc
+#BSUB -J NLIN[1-46]
 #BSUB -n 1
-#BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 24:00
-#BSUB -R "rusage[mem=32GB]"
+#BSUB -W 6:00
+#BSUB -R "rusage[mem=16GB]"
 #BSUB -o results/logs-%J-%I.out
 #BSUB -e results/logs_%J-%I.err
 # -- end of LSF options --
 
-nvidia-smi
+mkdir -p results
 
 module load python3/3.6.2
 module load numpy/1.13.1-python-3.6.2-openblas-0.2.20
 module load scipy/0.19.1-python-3.6.2
 module load matplotlib/2.0.2-python-3.6.2
-module load tensorflow/1.12-gpu-python-3.6.2
+module load tensorflow/1.12-cpu-python-3.6.2
 
-python3 run_inft.py # $ python3 <myScript.py>
+python3 hyper_AutoEncGeoShapDenseWDM.py -n $LSB_JOBINDEX
--- code/NN_train_1/sweepsubmitgpu2.sh
+++ code/NN_train_1/sweepsubmitgpu2.sh
@@ -1,6 +1,8 @@
+### SCRIPT FOR SUBSMISSION OF SWEEP JOBS TO GPU
+
 #!/bin/sh
 #BSUB -q gpuk40
-#BSUB -J NLIN[4-6]
+#BSUB -J NLIN[4-6]  $ [i-k]: numbers saved in local var LSB_JOBINDEX
 #BSUB -n 1
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
@@ -17,6 +19,8 @@ module load python3/3.6.2
 module load numpy/1.13.1-python-3.6.2-openblas-0.2.20
 module load scipy/0.19.1-python-3.6.2
 module load matplotlib/2.0.2-python-3.6.2
-module load tensorflow/1.10-gpu-python-3.6.2
+module load tensorflow/1.12-gpu-python-3.6.2
 
+# $ python3 <mySweepScript.py> -n $LSB_JOBINDEX
+# The last variable is the sweep variable that will be loaded in the script
 python3 sweep_inft.py -n $LSB_JOBINDEX
