055a6e50c8f23b932b5c126c6024f550c63a5f6c
NicolaDeRenzis <s161172@student.dtu.dk>
2019-02-26 10:01:51 +0100

    added trace filter for creating targets

--- code/matlab_code/decisionIdxCreation.m
+++ code/matlab_code/decisionIdxCreation.m
@@ -1,10 +1,19 @@
 ccc
 
-% signal generated path
-load('/work3/nidre/NN_receiver/generationSignal/10-43-28__rngSeed=1.mat');
+%% load trace
+traceFolder = '/work3/nidre/NN_receiver/generationSignal';
+traceFilter = struct(); % Set it to load a specific trace (if multiple matches, loads the first only
+traceFilter.OSNR = 20;
+traceFilter.rngSeed = 1;
+fileNames = traceFinder(traceFolder, traceFilter);
+param.txTrLoad.loadFileName = [traceFolder filesep fileNames(1).name];
 
+load(param.txTrLoad.loadFileName);
+
+%% extract info
 bits = logical(traceAndSetupParameters.txResults.txDiscrete.inputBitSequencesDiscrete.get);
 
 decisionIdx = bi2de(bits)+1;
 
-save('~/Code/NFT_NN_receiver/code/matlab_code/decisionMatrix.mat', 'decisionIdx');
+% save('~/Code/NFT_NN_receiver/code/matlab_code/decisionMatrix.mat', 'decisionIdx');
+save([strrep(param.txTrLoad.loadFileName,'.mat',''), '_decisionMatrix.mat'], 'decisionIdx');
